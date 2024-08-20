c6e98c64878ff17f66f079b3853b016f21ce7bd3
DLKReact@2024 <dlktechnologiesreact@gmail.com>
2024-08-20 10:25:04 +0530

    token changed

--- env/env.js
+++ env/env.js
@@ -1,7 +1,7 @@
 const env = {   
-    SECRET_TOKEN: "Pest123",
-    // DB_CONNECTION_URL: 'mongodb+srv://pestpatrol:Pest12345@cluster0.9zo8ck5.mongodb.net/dlk_clone?retryWrites=true&w=majority&appName=Cluster0'
-    DB_CONNECTION_URL:"mongodb://pestpatrol:Pest12345@ac-dqbhjfs-shard-00-00.9zo8ck5.mongodb.net:27017,ac-dqbhjfs-shard-00-01.9zo8ck5.mongodb.net:27017,ac-dqbhjfs-shard-00-02.9zo8ck5.mongodb.net:27017/?ssl=true&replicaSet=atlas-7n9anp-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+    SECRET_TOKEN: "Pest1234",
+    DB_CONNECTION_URL: 'mongodb+srv://pestpatrol:Pest12345@cluster0.9zo8ck5.mongodb.net/dlk_clone?retryWrites=true&w=majority&appName=Cluster0'
+    // DB_CONNECTION_URL:"mongodb://pestpatrol:Pest12345@ac-dqbhjfs-shard-00-00.9zo8ck5.mongodb.net:27017,ac-dqbhjfs-shard-00-01.9zo8ck5.mongodb.net:27017,ac-dqbhjfs-shard-00-02.9zo8ck5.mongodb.net:27017/?ssl=true&replicaSet=atlas-7n9anp-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
 }
 module.exports = env; 
              
